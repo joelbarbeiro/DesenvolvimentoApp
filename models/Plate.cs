bbc1e2fb5afc017ceb7e9270bc097af88a1d58f2
aebarroso <andre.e.barroso@gmail.com>
2024-06-08 15:04:20 +0100

    Base de dados menu com a relacao prato e extra correta

--- models/Plate.cs
+++ models/Plate.cs
@@ -18,7 +18,7 @@ namespace iCantine.models
         public int Stock { get; set; }
         public bool Active { get; set; }
         public double Price { get; set; }
-        public virtual ICollection<MenuPlate> MenuPlates { get; set; }
+        public virtual ICollection<Menu> Menu { get; set; }
         public virtual string DisplayName => $"Prato: {Description}€ - Tipo: {Type} - Stock: {Stock} - Price: {Price}";
         public virtual string ReservationName => $"{Description} - {Price}";
         public Plate()
